bcd5ce9bf7ea521effe73b4877b387d83acaa6e4
Jefferson Carley <jeff.carley@gmail.com>
2025-11-01 16:33:31 -0500

    adds fonts and configuration

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -8,10 +8,11 @@ unzip CascadiaMono.zip -d CascadiaFont
 cp CascadiaFont/*.ttf ~/.local/share/fonts
 rm -rf CascadiaMono.zip CascadiaFont
 
-wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
-unzip iafonts.zip -d iaFonts
-cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
-rm -rf iafonts.zip iaFonts
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d JetBrainsFont
+cp JetBrainsFont/*.ttf ~/.local/share/fonts
+rm -rf JetBrainsMono.zip JetBrainsFont
 
 fc-cache
 cd -
