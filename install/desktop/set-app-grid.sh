f488399dbe937075bfc40ff9b4cb7159f1f4c6b6
Jefferson Carley <jeff.carley@gmail.com>
2025-11-01 21:39:21 -0500

    updates

---
+++ install/desktop/set-app-grid.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Remove the ImageMagick icon
+sudo rm -rf /usr/share/applications/display-im6.q16.desktop
--- install/terminal/app-oh-my-zsh.sh
+++ install/terminal/app-oh-my-zsh.sh
@@ -5,6 +5,7 @@ sudo apt install -y zsh curl
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp ~/.local/share/mykub/configs/zshrc ~/.zshrc
-cp ~/.local/share/mykub/configs/zsh_aliases ~/.zsh_aliases
-cp ~/.local/share/mykub/configs/shell ~/.shell
+cp ~/.local/share/mykub/defaults/zshrc ~/.zshrc
+cp ~/.local/share/mykub/defaults/init ~/.zsh_init
+cp ~/.local/share/mykub/defaults/zsh_aliases ~/.zsh_aliases
+cp ~/.local/share/mykub/defaults/shell ~/.shell
